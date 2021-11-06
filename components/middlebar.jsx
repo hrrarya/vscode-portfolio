158024808c161b989d432b4e7a61baa228a84468
Hridoy Mozumder <hrrarya6@gmail.com>
2021-11-06 11:49:30 +0600

    removeing unwanted css selector

--- components/middlebar.jsx
+++ components/middlebar.jsx
@@ -21,62 +21,32 @@ const MiddleBar = () => {
       <div className="middle_bar__menu_wrapper cursor-pointer">
         <div className="flex items-center uppercase px-2" onClick={handleOpen}>
           {open ? (
-            <FaAngleDown size={15} color="white" className="" />
+            <FaAngleDown size={15} color="white" />
           ) : (
-            <FaAngleRight size={15} color="white" className="" />
+            <FaAngleRight size={15} color="white" />
           )}
           <span className="text-xs text-white tracking-wider">Portfolio</span>
         </div>
         {open && (
           <div className="middle_bar__menu_wrapper__menu flex flex-col text-white font-light text-xs">
             <NavLink href="/" classes={menu_class}>
-              <Image
-                src="/react_icon.svg"
-                alt="me"
-                width="15"
-                height="15"
-                style={{ marginRight: ".5rem" }}
-              />
+              <Image src="/react_icon.svg" alt="me" width="15" height="15" />
               home.jsx
             </NavLink>
             <NavLink href="/about" classes={menu_class}>
-              <Image
-                src="/html_icon.svg"
-                alt="me"
-                width="15"
-                height="15"
-                style={{ marginRight: ".5rem" }}
-              />
+              <Image src="/html_icon.svg" alt="me" width="15" height="15" />
               about.html
             </NavLink>
             <NavLink href="/contact" classes={menu_class}>
-              <Image
-                src="/css_icon.svg"
-                alt="me"
-                width="15"
-                height="15"
-                style={{ marginRight: ".5rem" }}
-              />
+              <Image src="/css_icon.svg" alt="me" width="15" height="15" />
               contact.css
             </NavLink>
             <NavLink href="/projects" classes={menu_class}>
-              <Image
-                src="/js_icon.svg"
-                alt="me"
-                width="15"
-                height="15"
-                style={{ marginRight: ".5rem" }}
-              />
+              <Image src="/js_icon.svg" alt="me" width="15" height="15" />
               projects.js
             </NavLink>
             <NavLink href="/" classes={menu_class}>
-              <Image
-                src="/markdown_icon.svg"
-                alt="me"
-                width="15"
-                height="15"
-                style={{ marginRight: ".5rem" }}
-              />
+              <Image src="/markdown_icon.svg" alt="me" width="15" height="15" />
               github.md
             </NavLink>
           </div>
